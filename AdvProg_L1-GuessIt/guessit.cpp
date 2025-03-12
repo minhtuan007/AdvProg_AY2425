30647a1a8bceac0539958d1e7e08f3c8d628c611
minhtuan007 <damminhtuan2006@gmail.com>
2025-03-12 14:30:17 +0700

    update

--- AdvProg_L1-GuessIt/guessit.cpp
+++ AdvProg_L1-GuessIt/guessit.cpp
@@ -14,7 +14,7 @@ using namespace std;
 ***/
 int generateRandomNumber() {
     // TODO: Return the random number in range 1 to 100
-    return 100;
+    return (rand() % 100) + 1;
 }
 
 
@@ -26,8 +26,10 @@ int generateRandomNumber() {
 ***/
 int getPlayerGuess() {
     // TODO: Ask the player guest and return the player's number
-
-    return 1;
+    cout << "Your guest number: ";
+    int getnum;
+    cin>>getnum;
+    return getnum;
 }
 
 
@@ -46,6 +48,9 @@ string getAnswer(int number, int randomNumber) {
               If number is equal randomNumber, the answer is "Congratulation! You win."
     ***/
     string answer;
+    if(number > randomNumber) answer = "Your number is higher.";
+    if(number < randomNumber) answer = "Your number is lower.";
+    if(number == randomNumber) answer = "Congratulation! You win.";
 
     return answer;
 }
@@ -59,8 +64,10 @@ string getAnswer(int number, int randomNumber) {
 ***/
 bool checkSuccess(string answer) {
     // TODO: return the result after checking that player guessed right or wrong
-    
-    return true;
+    if(answer == "Congratulation! You win."){
+        return true;
+    }
+    return false;
 }
 
 
@@ -73,6 +80,7 @@ bool checkSuccess(string answer) {
 bool checkContinuePlaying(char isContinued) {
     // TODO: return result after checking player continue playing or not
     bool result = false;
+    if(isContinued == 'Y') result = true;
 
     return result;
 }
@@ -87,7 +95,12 @@ bool checkContinuePlaying(char isContinued) {
 char getPlayerOpinion() {
     // TODO: Ask the player about continue playing and return the player's choice
     char isContinued;
-
+    cout<<"Y for continuing and N for stopping"<<endl;
+    cin >> isContinued;
+    while(isContinued != 'Y' && isContinued != 'N'){
+        cout << "Y for continuing and N for stopping";
+        cin>>isContinued;
+    }
     return isContinued;
 }
 
